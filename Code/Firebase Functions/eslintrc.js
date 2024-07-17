28686d0b9cb278ba65b4ef6b1f5eadd9e05843a6
Dung-Yi <dungyichao@gmail.com>
2024-07-17 09:52:01 -0400

    Add files via upload

---
+++ Code/Firebase Functions/eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+  ],
+  rules: {
+    quotes: ["error", "double"],
+    "no-unused-vars":"off",
+  },
+};
+
+// module.exports = {
+//   root: true,
+//   env: {
+//     es6: true,
+//     node: true,
+//   },
+//   parserOptions: {
+//     "ecmaVersion": 2018,
+//   },
+//   extends: [
+//     "eslint:recommended",
+//     "google",
+//   ],
+//   rules: {
+//     "no-restricted-globals": ["error", "name", "length"],
+//     "prefer-arrow-callback": "error",
+//     "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+//   },
+//   overrides: [
+//     {
+//       files: ["**/*.spec.*"],
+//       env: {
+//         mocha: true,
+//       },
+//       rules: {},
+//     },
+//   ],
+//   globals: {},
+// };
